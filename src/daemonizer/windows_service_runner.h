954de593676f4d8cef1b8a147de802775ef7238d
sispop <131583737+syssiproject@users.noreply.github.com>
2023-12-25 12:49:53 +0000

    daemonizer: don't uninstall windows service on exit

--- src/daemonizer/windows_service_runner.h
+++ src/daemonizer/windows_service_runner.h
@@ -147,8 +147,6 @@ namespace windows {
 
       on_state_change_request_(SERVICE_CONTROL_STOP);
 
-      // Ensure that the service is uninstalled
-      uninstall_service(m_name);
     }
 
     static void WINAPI on_state_change_request(DWORD control_code)
