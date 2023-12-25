736b11c6f1c8f1b05930bc041d91b3b56b1e3594
sispop <131583737+syssiproject@users.noreply.github.com>
2023-12-25 15:14:50 +0000

    Mainnet hardfork at height 86,097

--- src/cryptonote_basic/hardfork.cpp
+++ src/cryptonote_basic/hardfork.cpp
@@ -64,15 +64,16 @@ static uint8_t get_block_version(const cryptonote::block &b)
 // version 7 from the start of the blockchain, inhereted from Monero mainnet
 static constexpr HardFork::Params mainnet_hard_forks[] =
 {
-  { network_version_7,                      1,      0, 1503046577 },
-  { network_version_8,                      100,  0, 1692426487 },
-  { network_version_9_service_nodes,        102, 0, 1692426900 },
-  { network_version_10_bulletproofs,        110, 0, 1692427080 }, // 2018-12-13 23:30UTC
-  { network_version_11_infinite_staking,    118, 0, 1692427200 }, // 2019-03-26 13:00AEDT
-  { network_version_12_checkpointing,       120, 0, 1692427260 }, // 2019-07-24 14:00AEDT
-  { network_version_13_enforce_checkpoints, 125, 0, 1692427500 }, // 2019-10-23 19:00AEDT
-  { network_version_14_blink,               128, 0, 1692427620 }, // 2020-01-09 00:00UTC
-  { network_version_15_lns,                 130, 0, 1692427740 }, // 2020-03-25 14:00AEDT (03:00UTC)
+  { network_version_7,                      1,     0, 1503046577 },
+  { network_version_8,                      100,   0, 1692426487 },
+  { network_version_9_service_nodes,        102,   0, 1692426900 },
+  { network_version_10_bulletproofs,        110,   0, 1692427080 },
+  { network_version_11_infinite_staking,    118,   0, 1692427200 },
+  { network_version_12_checkpointing,       120,   0, 1692427260 },
+  { network_version_13_enforce_checkpoints, 125,   0, 1692427500 },
+  { network_version_14_blink,               128,   0, 1692427620 },
+  { network_version_15_lns,                 130,   0, 1692427740 },
+  {network_version_16,                      86097, 0, 1704076200},
 };
 
 static constexpr HardFork::Params testnet_hard_forks[] =
