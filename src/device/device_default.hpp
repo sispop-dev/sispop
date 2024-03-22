5054dd239c3918f63cce9b9fb1137dff71918552
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-03-22 15:10:06 +0000

    refactor: created new reference that matches structure

--- src/device/device_default.hpp
+++ src/device/device_default.hpp
@@ -113,6 +113,8 @@ namespace hw {
 
             bool  open_tx(crypto::secret_key &tx_key) override;
 
+            bool generate_output_ephemeral_keys(const size_t tx_version, const cryptonote::account_keys &sender_account_keys, const crypto::public_key &txkey_pub, const crypto::secret_key &tx_key, const cryptonote::tx_destination_entry &dst_entr, const boost::optional<cryptonote::account_public_address> &change_addr, const size_t output_index, const bool &need_additional_txkeys, const std::vector<crypto::secret_key> &additional_tx_keys, std::vector<crypto::public_key> &additional_tx_public_keys, std::vector<rct::key> &amount_keys, crypto::public_key &out_eph_public_key);
+
             bool  encrypt_payment_id(crypto::hash8 &payment_id, const crypto::public_key &public_key, const crypto::secret_key &secret_key) override;
 
             rct::key genCommitmentMask(const rct::key &amount_key) override;
