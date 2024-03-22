173e942aa2d4da01af88a7a2bbf77627089a2ac0
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-03-22 10:42:43 +0000

    fix: modified incomparable data types

--- src/device/device_default.cpp
+++ src/device/device_default.cpp
@@ -282,12 +282,13 @@ namespace hw {
             return true;
         }
 
-        bool device_default::generate_output_ephemeral_keys(const size_t tx_version, bool &found_change,
-                                                            const cryptonote::account_keys &sender_account_keys, const crypto::public_key &txkey_pub,  const crypto::secret_key &tx_key,
-                                                            const cryptonote::tx_destination_entry &dst_entr, const boost::optional<cryptonote::tx_destination_entry> &change_addr, const size_t output_index,
+        bool device_default::generate_output_ephemeral_keys(const size_t tx_version,
+                                                            const cryptonote::account_keys &sender_account_keys, const crypto::public_key &txkey_pub, const crypto::secret_key &tx_key,
+                                                            const cryptonote::tx_destination_entry &dst_entr, const boost::optional<cryptonote::account_public_address> &change_addr, const size_t output_index,
                                                             const bool &need_additional_txkeys, const std::vector<crypto::secret_key> &additional_tx_keys,
                                                             std::vector<crypto::public_key> &additional_tx_public_keys,
-                                                            std::vector<rct::key> &amount_keys,  crypto::public_key &out_eph_public_key) {
+                                                            std::vector<rct::key> &amount_keys, crypto::public_key &out_eph_public_key)
+        {
 
             // make additional tx pubkey if necessary
             cryptonote::keypair additional_txkey;
@@ -302,9 +303,8 @@ namespace hw {
 
             bool r;
             crypto::key_derivation derivation;
-            if (change_addr && *change_addr == dst_entr && !found_change)
+            if (change_addr && dst_entr.addr == *change_addr)
             {
-              found_change = true;
               // sending change to yourself; derivation = a*R
               r = generate_key_derivation(txkey_pub, sender_account_keys.m_view_secret_key, derivation);
               CHECK_AND_ASSERT_MES(r, false, "at creation outs: failed to generate_key_derivation(" << txkey_pub << ", " << sender_account_keys.m_view_secret_key << ")");
